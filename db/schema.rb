d810e60a1e2e286ac23537dbb687fa98ba319700
isoflabon <ksyo1skyan@gmail.com>
2019-09-22 18:21:12 +0900

    Add Folder Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_163834) do
+ActiveRecord::Schema.define(version: 2019_09_21_120004) do
+
+  create_table "folders", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
